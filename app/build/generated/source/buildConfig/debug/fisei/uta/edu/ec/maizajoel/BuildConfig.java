62b3ed83cc047404ee26ac858ea58c5f33d6b3dd
JoelMaiza97 <jmaiza0938@uta.edu.ec>
2022-05-17 20:56:50 -0500

    Primer Commit

---
+++ app/build/generated/source/buildConfig/debug/fisei/uta/edu/ec/maizajoel/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fisei.uta.edu.ec.maizajoel;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "fisei.uta.edu.ec.maizajoel";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
